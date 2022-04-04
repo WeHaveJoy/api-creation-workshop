54adb1b9cbbeb797a01ed735fcc2efdc53bd09db
Sinovuyo Dyantyi <sinovuyodyantyi@Sinovuyos-MacBook-Air.local>
2022-04-04 16:29:54 +0200

    added to the app.js

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ app.get('/api/garments', function(req, res){
 	// note that this route just send JSON data to the browser
 	// there is no template
 
-// app.get('/api/garments', function(req, res){
-
-	const gender = req.query.gender;
-	const season = req.query.season;
+	const gender = req.params.gender;
+	const season = req.params.season;
 
 	const filteredGarments = garments.filter(garment => {
 		// if both gender & season was supplied
@@ -29,17 +27,50 @@ app.get('/api/garments', function(req, res){
 		} else if(season != 'All') { // if season was supplied
 			return garment.season === season
 		}
+        //  if (gender === 'Female') {
+        //     return garment.gender === gender
+        // }
+        // else if (gender === 'Male') {
+        //     return garment.gender === gender
+        // }
+        // else if (season === 'Winter') {
+        //     return garment.season === season
+        // }
+        // else if (season === 'Summer') {
+        //     return garment.season === season
+        // }
+
 		return true;
 	});
 
 	// note that this route just send JSON data to the browser
 	// there is no template
-	res.json({ 
-		garments
-	});
-// });
-	// res.json({garments});
+	res.json({garments});
 });
+
+
+//  app.get('/api/garments/gender/:gender/season/:season', function(req, res){
+//     const gender = req.params.gender;
+// 	const season = req.params.season;
+//     const filteredGarments = garments.filter(garment => {
+//         if (gender === 'Female') {
+//             return garment.gender === gender
+//         }
+//         else if (gender === 'Male') {
+//             return garment.gender === gender
+//         }
+//         else if (season === 'Winter') {
+//             return garment.season === season
+//         }
+//         else if (season === 'Summer') {
+//             return garment.season === season
+//         }
+
+// 		return true;
+//     })
+//     res.json({garments: filteredGarments});
+// })
+
 
 app.get('/api/garments/price/:price', function(req, res){
 	const maxPrice = Number(req.params.price);
